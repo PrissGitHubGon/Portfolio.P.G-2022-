9a7bdf77d6729229f0d5b48df964dbe63226a054
priscillia <priscillia.g.pro@outlook.fr>
2022-07-29 16:08:59 +0200

    mise à jour

--- src/components/Header.js
+++ src/components/Header.js
@@ -76,7 +76,8 @@ function Header() {
                 </a>
               </li>
               <li className="border-b-2 border-fuchsia-900 sm:border-fuchsia-600  sm:hover:border-fuchsia-800 my-8 uppercase">
-                <a onClick={closeSideBar} href="tel:0672114122">
+                <a onClick={closeSideBar} href="tel:06">
+                  {/* obfuscation react telephone pour encrypter le numero */}
                   Appelez moi 😃
                 </a>
               </li>
