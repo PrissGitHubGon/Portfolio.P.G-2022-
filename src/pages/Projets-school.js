548fca2b3bff0931cbd916cb5d3a8de55338c46f
priscillia <priscillia.g.pro@outlook.fr>
2022-08-01 16:15:14 +0200

    MAJ code

--- src/pages/Projets-school.js
+++ src/pages/Projets-school.js
@@ -8,11 +8,11 @@ function ProjetsSchool() {
         return (
           <div
             key={index}
-            className=" text-center relative lg:w-52 lg:h-52 md:w-40 md:h-40   rounded-md w-24 h-24"
+            className=" text-center relative lg:w-52 lg:h-52 md:w-40 md:h-40   rounded-md w-24 h-24 cursor-pointer"
           >
             <a
-              href={realisation.idUrl}
-              className="h-full w-full aspect-square block bg-origin-padding bg-left-top bg-cover bg-no-repeat z-0"
+              href={realisation.link}
+              className="h-full w-full aspect-square block bg-origin-padding bg-left-top bg-cover bg-no-repeat z-0 "
               style={{ backgroundImage: `url(${realisation.imageUrl || ""})` }}
             >
               <img
@@ -20,12 +20,12 @@ function ProjetsSchool() {
                 alt={realisation.title}
                 className="w-full aspect-square hidden"
               />
+              <div className="h-full w-full aspect-square block absolute top-0 left-0 transition-opacity duration-300 opacity-0 hover:opacity-100 bg-gradient-to-r from-indigo-500/75 via-purple-500/75 to-pink-500/75 z-10">
+                <h3 className="text-white py-8 lg:py-20 px-3 mx-auto lg:text-3xl text-md">
+                  {realisation.title}
+                </h3>
+              </div>{" "}
             </a>
-            <div className="h-full w-full aspect-square block absolute top-0 left-0 transition-opacity duration-300 opacity-0 hover:opacity-100 bg-gradient-to-r from-indigo-500/75 via-purple-500/75 to-pink-500/75 z-10">
-              <h3 className="text-white py-8 lg:py-20 px-3 mx-auto lg:text-3xl text-md">
-                {realisation.title}
-              </h3>
-            </div>
           </div>
         );
       })}
