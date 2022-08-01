85736051377fb828e7f7ba4234406b376281def4
priscillia <priscillia.g.pro@outlook.fr>
2022-08-01 17:39:05 +0200

    MAJ code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,8 @@ function Home() {
   return (
     <>
       <main className="  ">
-        <div className="bg-black  flex flex-col sm:pt-10 lg:flex-row  ">
-          <div className=" lg:w-2/4 py-5 ml-22 ">
+        <div className="bg-black  flex flex-col py-16  lg:flex-row  ">
+          <div className=" lg:w-2/4  ml-22 pb-5 ">
             <img
               src="https://res.cloudinary.com/dl6flp50k/image/upload/v1659365893/me_c0bgw0.png"
               alt=""
@@ -14,7 +14,7 @@ function Home() {
             />
           </div>
 
-          <div className="bg-black lg:w-2/4 pl-3 pr-3 pb-5 sm:pt-3 2xl:w-3/4">
+          <div className="bg-black lg:w-2/4 pl-3 pr-3 pb-5  2xl:w-3/4">
             <h1 className="text-xl sm:px-12  md:text-3xl bg-clip-text text-transparent bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 ">
               <Typewriter
                 options={{
