e7a56d8c2b4b1cea1f791980fa1a54775e176c10
priscillia <priscillia.g.pro@outlook.fr>
2022-08-08 17:54:12 +0200

    ajout de contenu page Marvel

---
+++ src/components/Card.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+function Card(props) {
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col md:flex-row md:max-w-3xl rounded-lg bg-white shadow-lg">
+        <img
+          className=" w-full h-96 md:h-auto object-cover md:w-56 rounded-t-lg md:rounded-none md:rounded-l-lg"
+          src={props.images}
+          alt=""
+        />
+        <div className="p-6 flex flex-col justify-start">
+          <h5 className="text-gray-900 text-4xl font-medium mb-4 text-center">
+            {props.titleh5}
+          </h5>
+          <p className="text-gray-700 text-base mb-4">{props.text}</p>
+          <p className="text-gray-900 text-lg font-medium mb-2">
+            {props.text2}
+          </p>
+
+          <ol className="text-gray-700 text-base mb-4 ">
+            <li className="list-disc"> {props.li1} </li> <br />
+            <li className="list-disc">{props.li2}</li>
+            <br />
+            <li className="list-disc">{props.li3}</li>
+          </ol>
+          <h3 className="text-gray-900 text-lg font-medium mb-2">
+            {props.titleh3}
+          </h3>
+          <p className="text-gray-700 text-base pb-4 border-b-2">
+            {props.text3}
+          </p>
+          <div className="flex flex-row justify-center space-x-14 pt-5">
+            <a
+              href={props.gitHubLink}
+              className="text-white-800 text-2xl md:text-4xl"
+            >
+              <i className="fa-brands fa-github ml-3 md:ml-1 "></i>
+              <p className="text-center text-sm"> {props.gitHub} </p>
+            </a>
+            <a
+              href={props.siteWebLink1}
+              className="text-red-800 text-2xl md:text-4xl"
+            >
+              <i className="fa-brands ml-6 md:ml-4 fa-internet-explorer"></i>
+              <p className="text-center text-sm">{props.siteWeb}</p>
+            </a>
+            <a
+              href={props.siteWebLink2}
+              className="text-green-800 text-2xl md:text-4xl"
+            >
+              <i className="fa-brands fa-internet-explorer ml-7 md:ml-5"></i>
+              <p className="text-center text-sm">{props.siteWeb2}</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/pages/Marvel.js
+++ src/pages/Marvel.js
@@ -1,15 +1,33 @@
 import React from "react";
-import Iframe from "react-iframe";
+import Card from "../components/Card";
+// import Iframe from "react-iframe";
 function Marvel() {
   return (
     <a href="/projets-formation" className="cursor-pointer">
-      <div className="w-full md:w-full lg:w-full flex justify-center sm:pb-80 ">
-        <Iframe
+      <div className="w-full md:w-full lg:w-full flex justify-center sm:pb-80 md:mt-10">
+        {/* <Iframe
           url="https://embed.lottiefiles.com/animation/93663"
           id="myId"
           className="lottie"
           display="initial"
           position="relative"
+        /> */}
+        <Card
+          images="https://res.cloudinary.com/dl6flp50k/image/upload/v1659953418/gabriel-2519793_960_720_jnbquh.webp"
+          titleh5="Marvel"
+          text="Lors de mes 10 semaines de Bootcamp dans l'école de formation Le Reacteur, j'ai eu comme Test technique la réalisation du site Marvel."
+          text2="Lors de cet excercice je devais :"
+          li1="Créez un site en React (avec un routeur) qui contiendrait en haut : le logo MARVEL et un menu (personnages, comics, favoris)."
+          li2="Sur la page principale « personnages », je devais faire apparaître la liste des personnages MARVEL (100 par page), sous forme de fiche (photo, nom, description). En cliquant sur chaque fiche, il doit être possible d’accéder à une page regroupant les comics liés au personnage."
+          li3="Sur la page « comics », je devais faire apparaître la liste des comics MARVEL,par ordre alphabétique, sous forme de fiche (photo, titre, description)."
+          titleh3="Modification à venir :  "
+          text3="Intégrer la responsive / Intégrer un moteur de recherche, afin de rechercher par nom & titre / Créer un système permettant de mettre chaque fiche en favoris, en fonction des souhaits de l’utilisateur. /  Créez un système d’authentification par email et mot de passe."
+          gitHub="GitHub"
+          gitHubLink="https://github.com/PrissGitHubGon"
+          siteWeb="Site originel"
+          siteWebLink1="https://marvel-app-school.netlify.app/"
+          siteWeb2="Site en cours"
+          siteWebLink2="https://marvel-student-lereacteur.netlify.app/"
         />
       </div>
     </a>
