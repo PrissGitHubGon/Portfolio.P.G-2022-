1802e7b087c185ce053184ca494b0082513dcff8
priscillia <priscillia.g.pro@outlook.fr>
2023-04-07 16:26:32 +0200

    MAJ

--- src/components/components/Nav-desktop.js
+++ src/components/components/Nav-desktop.js
@@ -3,7 +3,7 @@ import { Translate } from "react-localize-alias";
 import { Link } from "react-router-dom";
 import DropdownContact from "./Dropdown";
 import { setActiveLanguage } from "react-localize-alias";
-import pdf from "../../assets/doc/cv-2023_Priscillia_Goncalves_dev_Front_React.pdf";
+import pdf from "../../assets/doc/cvPriscilliaGoncalves.pdf";
 
 const NavDesktop = () => {
   // const changeLanguage = () => {
